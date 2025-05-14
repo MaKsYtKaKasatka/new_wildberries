d15c28aac69d5f31b9884d264fc90d3f963df2d9
MaKsYtKaKasatka <student@Computer.>
2025-05-14 13:14:50 +0300

    create table and db

---
+++ app/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+
+
+class Product(models.Model):
+    class Meta:
+        verbose_name="Товар"
+        verbose_name_plural="Товары"
+    name = models.CharField(
+        verbose_name="Наименование",
+        max_length=200,
+    )
+    desc = models.TextField(
+        verbose_name="Описание",
+        max_length=600,
+    )
+    def __str__(self):
+        return self.name
+
+    price = models.DecimalField(verbose_name="Цена", max_digits=10, decimal_places=2)
+
+    image = models.ImageField(
+        verbose_name="Изображение",
+        upload_to="products/",
+    )
+    category = models.ForeignKey(
+        "Category",
+        verbose_name="Категория",
+        on_delete=models.CASCADE,
+    )
+    brand = models.ForeignKey(
+        "Brand",
+        verbose_name="Бренд",
+        on_delete=models.CASCADE,
+    )
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name="Категория"
+        verbose_name_plural="Категории"
+    name = models.CharField(
+        verbose_name="Название",
+        max_length=200,
+    )
+    parent = models.ForeignKey(
+        "self",
+        verbose_name="Родительская категория",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    class Meta:
+        verbose_name="Бренд"
+        verbose_name_plural="Бренды"
+    name = models.CharField(
+        verbose_name="Название",
+        max_length=200,
+    )
+    site_url = models.URLField(
+        verbose_name="Ссылка на сайт",
+        max_length=200,
+    )
+
+    country = models.CharField(
+        verbose_name="Страна",
+        max_length=200,
+    )
+    def __str__(self):
+        return self.name
